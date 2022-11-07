76725d939f6f858a0d34f1b7bbad2b189a439443
Sudharsan Babu Srikanthan <79599850+sudhsrik@users.noreply.github.com>
2022-11-07 17:52:46 -0500

    Add files via upload

---
+++ js/jsChallenges.js
@@ -0,0 +1,37 @@
+// Challenge 1
+// Update the toggleEmail() function so that when the checkbox is checked
+// the email field is displayed.
+
+function toggleEmail() {
+
+}
+
+
+// Challenge 2
+// Update the toggleAddress() function so that when the checkbox is checked
+// the home address field is hidden.
+
+function toggleAddress() {
+
+}
+
+
+// Challenge 3
+// Update the checkForm() function so that it checks whether or not you
+// have selected a radio button.
+
+function checkForm() {
+  // First, create a variable for each button so we don't have to keep typing
+  // document.getElementById
+  var optStrawberry = ;
+  var optBlueberry = ;
+
+  // Check the value of both variables to see if they're checked
+  if () {
+
+    // Return false to stop the form from submitting if one of the buttons
+    // isn't checked
+    return false;
+  }
+  return true;
+}
